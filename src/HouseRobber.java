e6c585fa78f152dc27761c797aae21f4fdc07ef1
ditto-d <denicakoni@gmail.com>
2025-11-06 22:43:14 -0500

    Initial commit

---
+++ src/HouseRobber.java
@@ -0,0 +1,31 @@
+ class HouseRobber {
+
+
+    public int rob(int[] nums) {
+        // empty array
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        // maxTwoBack: maximum money we could rob up to 2 houses ago
+        // maxOneBack: maximum money we could rob up to 1 house ago
+        int maxTwoBack = 0;
+        int maxOneBack = 0;
+
+        // proccesing each house
+        for (int num : nums) {
+            // we can either rob this house or skip it
+            // 1. we skip this house -> keep maxOneBack
+            // 2. rob this house -> take num + maxTwoBack
+            //(can't use maxOneBack because adjacent house)
+            int maxCurrent = Math.max(maxOneBack, maxTwoBack + num);
+
+            // sliding forward for the next iteration
+            maxTwoBack = maxOneBack;  // old "one back" becomes "two back"
+            maxOneBack = maxCurrent;   // current max becomes "one back"
+        }
+
+        // after processing all houses, maxOneBack contains the answer
+        return maxOneBack;
+    }
+}
